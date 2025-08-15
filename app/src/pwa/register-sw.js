0f79e116bbeac6709732ab7ea6f25d58d65f8fde
Henry LeCates <hglecates@gmail.com>
2025-08-15 18:55:23 -0400

    fix: clean old

--- app/src/pwa/register-sw.js
+++
@@ -1,5 +0,0 @@
-if ('serviceWorker' in navigator && import.meta.env.PROD) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js').catch(() => {});
-  });
-}
